d13f75d88fd4acabeea777b2ca37462150b68690
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-08-12 11:59:55 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by KPS on 8/12/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> c1= new ArrayList<String>();
+        c1.add("Red");
+        c1.add("Green");
+        c1.add("Black");
+        c1.add("White");
+        c1.add("Pink");
+        System.out.println("Original array list: " + c1);
+        ArrayList<String> newc1 = (ArrayList<String>)c1.clone();
+        System.out.println("Cloned array list: " + newc1);
+    }
+}
